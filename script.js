ebde009d8b933296ed61f9e8263704844029d7fc
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-05-19 17:52:08 +0100

    js effects added

---
+++ script.js
@@ -0,0 +1,13 @@
+// Array.from(document.querySelectorAll
+//     (".navigation-button")).forEach((item) => {
+//   item.onClick = () => {
+//     item.parentElement.parentElement.classList.
+//     toggle("change");
+//   };
+// });
+
+Array.from(document.querySelectorAll(".navigation-button")).forEach((item) => {
+  item.addEventListener("click", () => {
+    item.parentElement.parentElement.classList.toggle("change");
+  });
+});
