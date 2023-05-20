42572b13f80bb455e2da6eea989f33f705c86b3a
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-05-20 17:50:33 +0100

    navbar finished

--- script.js
+++ script.js
@@ -12,6 +12,43 @@
 // item.getElementsByClassName.cssText = `background-color:${colors[i++]}`
 // })
 
+// const container = document.querySelector(".container");
+
+// document.querySelector(".open-navbar-icon").addEventListener("click", () => {
+//   container.classList.add("change");
+// });
+
+// document.querySelector(".close-navbar-icon").addEventListener("click", () => {
+//   container.classList.remove("change");
+// });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const navbarWrapper = document.querySelector(".navbar-wrapper");
+  const openNavbarIcon = document.querySelector(".open-navbar-icon");
+  const closeNavbarIcon = document.querySelector(".close-navbar-icon");
+
+  openNavbarIcon.addEventListener("click", () => {
+    navbarWrapper.classList.add("change");
+  });
+
+  closeNavbarIcon.addEventListener("click", () => {
+    navbarWrapper.classList.remove("change");
+  });
+
+  // Close navbar when clicking outside of it
+  document.addEventListener("click", (event) => {
+    const targetElement = event.target;
+    if (
+      !navbarWrapper.contains(targetElement) &&
+      !targetElement.classList.contains("open-navbar-icon")
+    ) {
+      navbarWrapper.classList.remove("change");
+    }
+  });
+});
+
+
+
 let i = 0;
 
 const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
