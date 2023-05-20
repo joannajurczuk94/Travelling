c0d01d5b8ce1e0fda43a86057624c6e640a75da4
Joanna <105747071+joannajurczuk94@users.noreply.github.com>
2023-05-20 13:58:20 +0100

    navbar colors added

--- script.js
+++ script.js
@@ -6,6 +6,23 @@
 //   };
 // });
 
+// const color = ['#6495ed', '#7fffd4', '#ffa07a', '#f08080', '#afeeee'];
+// let i = 0;
+// Array.from(document.querySelectorAll(".nav-link")).forEach((item) => {
+// item.getElementsByClassName.cssText = `background-color:${colors[i++]}`
+// })
+
+let i = 0;
+
+const colors = ["#6495ed", "#7fffd4", "#ffa07a", "#f08080", "#afeeee"];
+
+Array.from(document.querySelectorAll(".nav-link")).forEach((item, index) => {
+  item.style.backgroundColor = colors[index % colors.length];
+});
+
+
+
+
 Array.from(document.querySelectorAll(".navigation-button")).forEach((item) => {
   item.addEventListener("click", () => {
     item.parentElement.parentElement.classList.toggle("change");
